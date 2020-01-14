86953eb3e8843350d0079347460af0623d3d98bb
Yizhou Qian <adncat@stanford.edu>
2020-01-14 11:32:32 -0800

    Update main_q3.cpp

--- main_q3.cpp
+++ main_q3.cpp
@@ -5,6 +5,19 @@
 // TODO: add your function here //
 
 
+unsigned int CountData(const std::set<double>& data, double low, double high) {
+
+    if (low > high) {std::cerr<<"Low value higher than high value!\n"}
+    std::set<double>::iterator it1=data.lower_bound (low);                //       ^
+    std::set<double>::iterator it2=data.upper_bound (high); 
+    std::set<double>::iterator it;
+    unsigned int count=0;
+    for (it=it1; it != it2; it++) {
+        count++;
+    }
+    return count;
+}
+
 int main()
 {
     // Test with N(0,1) data.
@@ -16,6 +29,6 @@ int main()
         data.insert(distribution(generator));
 
     // TODO: print out number of points in [2, 10] //
-
+    std::cout<<CountData(data,0.2,0.8)<<" values between 0.2 and 0.8.\n";
     return 0;
 }
