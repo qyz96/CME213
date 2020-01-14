5f293719ec309d49c84165edb1b8840c97a4de53
Yizhou Qian <adncat@stanford.edu>
2020-01-14 09:23:35 -0800

    Update main_q1.cpp

--- main_q1.cpp
+++ main_q1.cpp
@@ -15,12 +15,6 @@ int main()
     }
     
     Matrix<double> mat3=mat1+mat2;
-    for (unsigned int i=0; i<n; i++){
-        for (unsigned int j=0; j<n; j++){
-            cout<<mat3(i,j)<<" ";
-        }
-        //cout<<"\n";
-    }
     cout<<mat3;
     std::cout<<"L0 norm is "<<mat1.l0norm()<<"\n";
     return 0;
