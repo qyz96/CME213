967029a8462b618a2655dad8f463ebd78cc40a64
Yizhou Qian <adncat@stanford.edu>
2020-01-14 08:27:44 -0800

    Update main_q1.cpp

--- main_q1.cpp
+++ main_q1.cpp
@@ -14,7 +14,7 @@ int main()
         }
     }
     
-    MatrixSymmetric<double> mat3=mat1+mat2;
+    Matrix<double> mat3=mat1+mat2;
     for (unsigned int i=0; i<n; i++){
         for (unsigned int j=0; j<n; j++){
             //cout<<mat1(i,j)<<" ";
