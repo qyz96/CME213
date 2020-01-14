fc1fbc7500e09769612b816646a69f41c6b55123
Yizhou Qian <adncat@stanford.edu>
2020-01-14 14:39:48 -0800

    k

--- main_q3.cpp
+++ main_q3.cpp
@@ -23,12 +23,14 @@ int main()
     // Test with N(0,1) data.
     std::cout << "Generating N(0,1) data" << std::endl;
     std::set<double> data;
+    double low=0.2;
+    double high=0.8;
     std::default_random_engine generator;
     std::normal_distribution<double> distribution(0.0, 1.0);
     for (unsigned int i = 0; i < 1000; ++i)
         data.insert(distribution(generator));
 
     // TODO: print out number of points in [2, 10] //
-    std::cout<<CountData(data,0.2,0.8)<<" values between 0.2 and 0.8.\n";
+    std::cout<<CountData(data,low, high)<<" values between "<<low<<" and "<<high<<".\n";
     return 0;
 }
