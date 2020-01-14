9cfb9bb03f8519d936ea0c0fe5751554da953dd2
Yizhou Qian <adncat@stanford.edu>
2020-01-13 22:25:40 -0800

    Update matrix.hpp

--- matrix.hpp
+++ matrix.hpp
@@ -27,8 +27,8 @@ class Matrix
         return os;
     }
 
-    /*
-    Matrix<T>& operator + (const Matrix<T>& mat1) const {
+    
+    Matrix<T> operator + (const Matrix<T>& mat1) const {
         Matrix<T> output(mat1.Size());
         if (mat1.Size()!=this->Size()) {
             cout << mat1.Size() << " " << this->Size() << "\n";
@@ -43,7 +43,7 @@ class Matrix
         return output;
     }
 
-    */
+    
         unsigned int l0norm() {
         unsigned int norm=0;
         for (unsigned int i=0; i<size; i++) {
