cd76ab82f2c93f3edf40750b0f75b60b7e9849e5
Yizhou Qian <adncat@stanford.edu>
2020-01-13 20:12:11 -0800

    Update matrix.hpp

--- matrix.hpp
+++ matrix.hpp
@@ -11,7 +11,7 @@ class Matrix
     public :
     Matrix(unsigned int n): size(n) {}
     T& Entry(unsigned int i, unsigned int j) {};
-    //const T& Entry(unsigned int i, unsigned int j) const{};
+    const T& Entry(unsigned int i, unsigned int j) const{};
     const T& operator ()(unsigned int i, unsigned int j) const { return Entry(i,j);}
     T& operator ()(unsigned int i, unsigned int j) { return Entry(i,j);}
 
