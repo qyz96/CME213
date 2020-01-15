a058ddac5813424937c62cd3396a98389ba26faf
Yizhou Qian <adncat@stanford.edu>
2020-01-15 11:15:41 -0800

    Update main_q1.cpp

--- main_q1.cpp
+++ main_q1.cpp
@@ -14,8 +14,8 @@ bool VerifySymmetry(const MatrixSymmetric<T>& data) {
 
 template <typename T, typename F>
 void AssignVal(MatrixSymmetric<T>& data, F f) {
-    for (unsigned int j=0; j<data.Size(); j++) {
-        for (unsigned int i=j; i<data.Size(); i++) {
+    for (int j=0; j<data.Size(); j++) {
+        for (int i=j; i<data.Size(); i++) {
             data(i,j)=f(i,j);
         }
     }
@@ -30,8 +30,8 @@ int main()
     MatrixSymmetric<double> mat_small(n);
     MatrixSymmetric<double> mat_small2(n);
     MatrixSymmetric<double> mat_large(2*n);  
-    auto f1=[](unsigned int i, unsigned int j) {return (double)(i+j+1);};
-    auto f2=[](unsigned int i, unsigned int j){return (double)(i-j-3);};
+    auto f1=[](int i, int j) {return (i+j+1);};
+    auto f2=[](int i, int j){return (i-j-3);};
     AssignVal(mat_small, f1);
     AssignVal(mat_small2, f2);
     AssignVal(mat_large, f2);
