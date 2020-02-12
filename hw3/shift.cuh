5ea95b037e4c9bbaaadc51cf815963f895a447b5
Yizhou Qian <adncat@stanford.edu>
2020-02-12 12:47:45 -0800

    Update shift.cuh

--- hw3/shift.cuh
+++ hw3/shift.cuh
@@ -22,7 +22,7 @@ void shift_char(const uchar *input_array, uchar *output_array,
 
     const int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i<array_length) {
-        output_array[i]=2;
+        output_array[1]=2;
     }
 
 }
