d7e2c1fc7f3c414f93417a97fcc928b3bbed500e
Yizhou Qian <adncat@stanford.edu>
2020-02-12 22:32:32 -0800

    Update pagerank.cuh

--- hw3/pagerank.cuh
+++ hw3/pagerank.cuh
@@ -23,7 +23,7 @@ __global__ void device_graph_propagate(
 
     const uint i = (uint)(blockIdx.x * blockDim.x + threadIdx.x);
     if (i<num_nodes) {
-        float sum = 0.f;
+        float sum = 0;
 
         for (uint j = graph_indices[i]; j < graph_indices[i + 1]; j++)
         {
@@ -31,7 +31,7 @@ __global__ void device_graph_propagate(
             if (i==0) printf("gpu_index_%d, %d, %d\n", graph_edges[j], j, graph_indices[i+1]);
             if (i==0) printf("gpu_%f, %f\n", graph_nodes_in[graph_edges[j]], inv_edges_per_node[graph_edges[j]]);
         }
-        graph_nodes_out[i] = 0.5f / (float)num_nodes + 0.5f * sum;
+        graph_nodes_out[i] = 0.5 / (float)num_nodes + 0.5 * sum;
         if (i==0) printf("gpu_val_%d\n", graph_nodes_out[i]);
     }
 
