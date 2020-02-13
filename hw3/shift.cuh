8b9b2fc19a28e269900903a2fc31d803c27719af
Yizhou Qian <adncat@stanford.edu>
2020-02-12 18:14:09 -0800

    Update shift.cuh

--- hw3/shift.cuh
+++ hw3/shift.cuh
@@ -88,7 +88,7 @@ double doGPUShiftUInt(const uchar *d_input, uchar *d_output,
     uint new_shift_amount=0;
     new_shift_amount+=(shift_amount);
     for (uint k=0; k<3; k++) {
-        new_shift_amount=(new_shift_amount<<=8);
+        new_shift_amount=(new_shift_amount<<8);
         new_shift_amount+=shift_amount;
     }
     event_pair timer;
