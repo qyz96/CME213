8bc109f99dbd33902a8c06a8f2ce52e50b345c05
Yizhou Qian <adncat@stanford.edu>
2020-02-24 23:12:12 -0800

    Update gpuStencil.cu

--- hw4/gpuStencil.cu
+++ hw4/gpuStencil.cu
@@ -204,7 +204,6 @@ double gpuComputationBlock(Grid& curr_grid, const simParams& params) {
 
     int gx = params.gx();
     // TODO: Declare variables/Compute parameters.
-    int numYPerStep = 16;
     int block_size_x = 512;
     int block_size_y = 1;
     int numBlocks_x = (nx + block_size_x - 1) / block_size_x;
