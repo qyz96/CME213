904cade18ab4ea6230f0d2daa8945c7326e2e086
Yizhou Qian <adncat@stanford.edu>
2020-03-11 03:26:38 -0700

    Update dns.h

--- hw5/dns.h
+++ hw5/dns.h
@@ -285,7 +285,9 @@ void dns_multiply(const struct mesh_info& mesh_info, const float *a,
   }
 
   // TODO: Release any resources that you may have allocated
+  if (rank==0) {
   print_mat(c, matrix_dim);
+  }
 
   delete[] Aik;
   delete[] Bkj;
