550e28d49eac130b5b08a09d5f84512cf704d757
Yizhou Qian <adncat@stanford.edu>
2020-03-14 22:20:56 -0700

    Update tests.cpp

--- final_project/utils/tests.cpp
+++ final_project/utils/tests.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 #define SCALE 1         // Factor to SCALE the GEMM problem size by
-#define NUM_ITERS 10    // Number of GEMMs run for timing purposes
+#define NUM_ITERS 1    // Number of GEMMs run for timing purposes
 #define GEMM_TOL 1e-12  // Tolerance for GEMM comparison
 
 // check whether the matrix from Seq is the same as from Par.
