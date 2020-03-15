a5ba4e757777d5570a3d8b9d9a3b87f85143c1ac
Yizhou Qian <adncat@stanford.edu>
2020-03-14 19:02:51 -0700

    Update tests.cpp

--- final_project/utils/tests.cpp
+++ final_project/utils/tests.cpp
@@ -138,9 +138,9 @@ void TestGEMM(int M, int N, int K) {
     double beta = 5.0;
 
     int num_iters = 100;
-    K=512;
-    M=512;
-    N=512;
+    K=32;
+    M=32;
+    N=32;
 
     A = (double*)malloc(M*K*sizeof(double));
     B = (double*)malloc(K*N*sizeof(double));
