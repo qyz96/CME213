6f9c7cb8a9916529e3f72077a3ddef111f844b0e
Yizhou Qian <adncat@stanford.edu>
2020-03-15 13:44:42 -0700

    Update gpu_func.cu

--- final_project/gpu_func.cu
+++ final_project/gpu_func.cu
@@ -168,7 +168,7 @@ void device_gemm_shared3(double* __restrict__ A, double* __restrict__ B,
                 if ((ii+BLOCK_SIZE_X*m)>=K) {
                     break;
                 }
-                Bs[ii]=B[ii+BLOCK_SIZE_X*m+N*j];
+                Bs[ii]=B[ii+BLOCK_SIZE_X*m+K*j];
                 printf("Bs[%d]=B(%d, %d)=%f\n", ii, ii+BLOCK_SIZE_X*m, j, B[ii+BLOCK_SIZE_X*m+N*j]);
             }
         }
