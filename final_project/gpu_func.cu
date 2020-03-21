42409ec80d1335d2c5b596cb878b213efde6f5e1
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-03-21 00:45:08 -0700

    test

--- final_project/gpu_func.cu
+++ final_project/gpu_func.cu
@@ -338,13 +338,13 @@ void gpu_feedforward(NeuralNetwork& nn, const arma::mat& X, struct cache& cache)
 
 
     
-    //cache.z.resize(2);
+    cache.z.resize(2);
     std::cout<<"Resizing....\n";
-    cache.z.push_back(arma::zeros<arma::mat>(K, num_sample));
-    //cache.z[1]=arma::zeros<arma::mat>(N, num_sample);
-    //cache.a.resize(2);
-    //cache.a[0]=arma::zeros<arma::mat>(K, num_sample);
-    //cache.a[1]=arma::zeros<arma::mat>(N, num_sample);
+    cache.z[0]=arma::zeros<arma::mat>(K, num_sample);
+    cache.z[1]=arma::zeros<arma::mat>(N, num_sample);
+    cache.a.resize(2);
+    cache.a[0]=arma::zeros<arma::mat>(K, num_sample);
+    cache.a[1]=arma::zeros<arma::mat>(N, num_sample);
 
     double* a0;
     double* a1;
