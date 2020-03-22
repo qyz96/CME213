a6a6fb5a5d09bb953116460e059f07fe45b89a8e
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-03-22 02:27:06 -0700

    Update gpu_func.cu

--- final_project/gpu_func.cu
+++ final_project/gpu_func.cu
@@ -50,9 +50,9 @@ void device_gemm(double* __restrict__ A, double* __restrict__ B,
             double left = A_T ? A[k + i * K] : A[i + k * M];
             double right = B_T ? B[j + k * N] : B[k + j * K];
             temp+=left*right;
-            if ((i==0) && (j==0)) {
+/*             if ((i==0) && (j==0)) {
                 if (k<=5) printf("A[%d,%d]=%f\n", i, k, A[i + k * M]);
-            }
+            } */
         }
         C[i + j * M]=alpha*temp+beta*C[i+j*M];
     }
