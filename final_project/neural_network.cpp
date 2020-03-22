867b6a8075034a0f4d00a766e234e2310b117729
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-03-22 03:19:16 -0700

    Update neural_network.cpp

--- final_project/neural_network.cpp
+++ final_project/neural_network.cpp
@@ -693,6 +693,12 @@ void parallel_train(NeuralNetwork& nn, const arma::mat& X, const arma::mat& y,
             } */
 
             gpu_updatecoeffcient(nn, bpgrads, learning_rate);
+
+
+            std::cout<<"nnW0: "<<nn.W[0].submat(0,0, 5, 5)<<"\n";
+            std::cout<<"nnW1: "<<nn.W[1].submat(0,0, 5, 5)<<"\n";
+            std::cout<<"nnb0: "<<nn.W[0].subvec(0,, 5)<<"\n";
+            std::cout<<"nnb1: "<<nn.W[1].subvec(0, 5)<<"\n";
             //std::cout<<"Subtracting gradient done...\n";
             if(print_every <= 0) {
                 print_flag = batch == 0;
