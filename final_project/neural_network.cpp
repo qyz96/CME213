311b56d7f9b846084617fcc6693e08f05020a669
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-03-22 15:02:16 -0700

    Update neural_network.cpp

--- final_project/neural_network.cpp
+++ final_project/neural_network.cpp
@@ -669,7 +669,6 @@ void parallel_train(NeuralNetwork& nn, const arma::mat& X, const arma::mat& y,
             std::cout<<"b0: "<<bpgrads.db[0].subvec(0, 5)<<"\n";
             std::cout<<"b1: "<<bpgrads.db[1].subvec(0, 5)<<"\n"; */
             //std::cout<<"Backpropagation done...\n";
-/*             print_every=1;
             if(print_every > 0 && iter % print_every == 0) {
                 if(grad_check) {
                     struct grads numgrads;
@@ -680,7 +679,7 @@ void parallel_train(NeuralNetwork& nn, const arma::mat& X, const arma::mat& y,
                 std::cout << "Loss at iteration " << iter << " of epoch " << epoch << "/" <<
                           epochs << " = " << loss(nn, bpcache.yc, y_batch, reg) << "\n";
                 return;
-            } */
+            }
 
 
             //std::cout<<"Subtracting gradient...\n";
