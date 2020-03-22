704dcf9b76a8f64b907f3e8c741474ff738914c2
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-03-21 23:59:11 -0700

    Update neural_network.cpp

--- final_project/neural_network.cpp
+++ final_project/neural_network.cpp
@@ -356,7 +356,7 @@ void my_feedforward(NeuralNetwork& nn, const arma::mat& X, struct cache& cache,
     cudaMemcpy(a1, da1, sizeof(double) * N * num_sample, cudaMemcpyDeviceToHost);
     cudaMemcpy(yc, da1, sizeof(double) * N * num_sample, cudaMemcpyDeviceToHost);
     
-/*     cudaFree(dz0);
+    cudaFree(dz0);
     cudaFree(dz1);
     cudaFree(da0);
     cudaFree(da1);
@@ -365,7 +365,7 @@ void my_feedforward(NeuralNetwork& nn, const arma::mat& X, struct cache& cache,
     cudaFree(db0);
     cudaFree(db1);
     cudaFree(dX);
-    cudaFree(dexp); */
+    cudaFree(dexp);
 
 }
 
@@ -584,13 +584,13 @@ void parallel_train(NeuralNetwork& nn, const arma::mat& X, const arma::mat& y,
 
 
             feedforward(nn, X_batch, bpcache);
-/*             std::cout<<"nnW0: "<<nn.W[0].submat(0,0, 5, 5)<<"\n";
+            std::cout<<"nnW0: "<<nn.W[0].submat(0,0, 5, 5)<<"\n";
             std::cout<<"nnW1: "<<nn.W[1].submat(0,0, 5, 5)<<"\n";
             std::cout<<"z0: "<<bpcache.z[0].submat(0,0, 5, 5)<<"\n";
             std::cout<<"a0: "<<bpcache.a[0].submat(0,0,5,5)<<"\n";
             std::cout<<"z1: "<<bpcache.z[1].submat(0,0,5,5)<<"\n";
             std::cout<<"a1: "<<bpcache.a[1].submat(0,0,5,5)<<"\n";
-            std::cout<<"y: "<<bpcache.yc.submat(0,0, 5, 5)<<"\n"; */
+            std::cout<<"y: "<<bpcache.yc.submat(0,0, 5, 5)<<"\n";
             struct grads bpgrads;
             //std::cout<<"Backpropagation begins...\n";
             backprop(nn, y_batch, reg, bpcache, bpgrads);
