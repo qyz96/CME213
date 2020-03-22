960dcf60342a68ee786ca96ec0ea90157c58af2b
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-03-22 03:19:26 -0700

    Update neural_network.cpp

--- final_project/neural_network.cpp
+++ final_project/neural_network.cpp
@@ -709,7 +709,7 @@ void parallel_train(NeuralNetwork& nn, const arma::mat& X, const arma::mat& y,
             /* Following debug routine assumes that you have already updated the arma
                matrices in the NeuralNetwork nn.  */
 
-            debug=1;
+            debug=0;
             if(debug && rank == 0 && print_flag) {
                 write_diff_gpu_cpu(nn, iter, error_file);
             }
