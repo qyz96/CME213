ab5896e3060c322c7d1f66885698119ceeaf719c
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-03-27 19:08:50 -0700

    Update neural_network.cpp

--- final_project/neural_network.cpp
+++ final_project/neural_network.cpp
@@ -356,24 +356,24 @@ class OneBatchUpdate2  {
         MPI_SAFE_CALL(MPI_Bcast(&totalsize, 1, MPI_INT, 0, MPI_COMM_WORLD));
         
         
-/*         double* xdata=(double*)malloc(sizeof(double)*M*totalsize);
+        double* xdata=(double*)malloc(sizeof(double)*M*totalsize);
         double* ydata=(double*)malloc(sizeof(double)*totalsize*N);
         
         if (rank == 0) {
             cudaMemcpy(xdata, X.memptr(), sizeof(double) * M * totalsize, cudaMemcpyHostToHost);
             cudaMemcpy(ydata, y.memptr(), sizeof(double) * N * totalsize, cudaMemcpyHostToHost);
         }
-        */
+       
         cudaMalloc((void**)&dX, sizeof(double) * M * totalsize);
         cudaMalloc((void**)&dY, sizeof(double) * N * totalsize);
-       /*  MPI_SAFE_CALL(MPI_Bcast(xdata, M*totalsize, MPI_DOUBLE, 0, MPI_COMM_WORLD));
-        MPI_SAFE_CALL(MPI_Bcast(ydata, N*totalsize, MPI_DOUBLE, 0, MPI_COMM_WORLD));      */
+/*        /*  MPI_SAFE_CALL(MPI_Bcast(xdata, M*totalsize, MPI_DOUBLE, 0, MPI_COMM_WORLD));
+        MPI_SAFE_CALL(MPI_Bcast(ydata, N*totalsize, MPI_DOUBLE, 0, MPI_COMM_WORLD));      */ */
 
         //std::cout<<"X: \n"<<X.submat(0,0,5,5);
-        cudaMemcpy(dX, X.memptr(), sizeof(double) * M * totalsize, cudaMemcpyHostToDevice);
-        cudaMemcpy(dY, y.memptr(), sizeof(double) * N * totalsize, cudaMemcpyHostToDevice);
-/*         free(xdata);
-        free(ydata); */
+        cudaMemcpy(dX, xdata, sizeof(double) * M * totalsize, cudaMemcpyHostToDevice);
+        cudaMemcpy(dY, ydata, sizeof(double) * N * totalsize, cudaMemcpyHostToDevice);
+        free(xdata);
+        free(ydata);
 
 
 
