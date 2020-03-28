cfba3d1743822b44e83a39d84367ce0c668ce58c
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-03-27 21:21:20 -0700

    Update neural_network.cpp

--- final_project/neural_network.cpp
+++ final_project/neural_network.cpp
@@ -908,9 +908,7 @@ void parallel_train(NeuralNetwork& nn, const arma::mat& X, const arma::mat& y,
             if(debug && rank == 0 && print_flag) {
                 write_diff_gpu_cpu(nn, iter, error_file);
             }
-/*             if (iter > 5) {
-                return;
-            } */
+
             iter++;
 
         }
