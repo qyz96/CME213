936b3762412c4f9e92b911b5d0b60fa4018e5c2b
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-03-29 04:00:17 -0700

    Update gpu_func.cu

--- final_project/gpu_func.cu
+++ final_project/gpu_func.cu
@@ -274,20 +274,20 @@ int myGEMM(double* __restrict__ A, double* __restrict__ B,
     double al=*alpha;
     double be=*beta;
     
-/*     int block_size_x = BLOCK_SIZE;
+    int block_size_x = BLOCK_SIZE;
     int block_size_y = BLOCK_SIZE;
     int numBlocks_x = (N + block_size_x - 1) / block_size_x;
-    int numBlocks_y = (M + block_size_y - 1) / (block_size_y); */
+    int numBlocks_y = (M + block_size_y - 1) / (block_size_y);
     
-    int block_size_x = BLOCK_SIZE_X;
+/*     int block_size_x = BLOCK_SIZE_X;
     int block_size_y = BLOCK_SIZE_Y;
     int numBlocks_x = (N + block_size_x - 1) / (block_size_x);
     //int numBlocks_x = (N + block_size_y * block_size_x - 1) / (block_size_x * block_size_y);
-    int numBlocks_y = (M + block_size_y * block_size_x - 1) / (block_size_x * block_size_y);
+    int numBlocks_y = (M + block_size_y * block_size_x - 1) / (block_size_x * block_size_y); */
     //printf("myGEMM is called!\n");
     dim3 threads(block_size_x, block_size_y);
     dim3 blocks(numBlocks_x, numBlocks_y);
-    device_gemm_shared2<<<blocks, threads>>>(A, B, C, al, be, M, N, K);
+    device_gemm_shared<<<blocks, threads>>>(A, B, C, al, be, M, N, K);
 
     
     return 0;
