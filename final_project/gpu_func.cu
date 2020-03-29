a3a55f5bd2a491b6ddd83650d5871b9d180a8c83
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-03-29 03:36:53 -0700

    Update gpu_func.cu

--- final_project/gpu_func.cu
+++ final_project/gpu_func.cu
@@ -8,7 +8,7 @@
 #include "cublas_v2.h"
 #define BLOCK_SIZE 32
 #define BLOCK_SIZE_X 16
-#define BLOCK_SIZE_Y 4
+#define BLOCK_SIZE_Y 8
 #define BLOCK_SIZE_K 32
 __global__
 void device_add_one(int* d_result, int t) {
@@ -69,8 +69,8 @@ void device_gemm_shared(double* __restrict__ A, double* __restrict__ B,
         int rj = threadIdx.x;
         int ri = threadIdx.y;
         double temp=0;
-        __shared__ double As[BLOCK_SIZE][BLOCK_SIZE+1];
-        __shared__ double Bs[BLOCK_SIZE][BLOCK_SIZE+1];
+        __shared__ double As[BLOCK_SIZE][BLOCK_SIZE];
+        __shared__ double Bs[BLOCK_SIZE][BLOCK_SIZE];
         //__shared__ double Accumu[BLOCK_SIZE][BLOCK_SIZE];
         
         int nb = (K+BLOCK_SIZE-1)/BLOCK_SIZE;
@@ -274,20 +274,20 @@ int myGEMM(double* __restrict__ A, double* __restrict__ B,
     double al=*alpha;
     double be=*beta;
     
-    int block_size_x = BLOCK_SIZE;
+/*     int block_size_x = BLOCK_SIZE;
     int block_size_y = BLOCK_SIZE;
     int numBlocks_x = (N + block_size_x - 1) / block_size_x;
-    int numBlocks_y = (M + block_size_y - 1) / (block_size_y);
+    int numBlocks_y = (M + block_size_y - 1) / (block_size_y); */
     
-/*     int block_size_x = BLOCK_SIZE_X;
+    int block_size_x = BLOCK_SIZE_X;
     int block_size_y = BLOCK_SIZE_Y;
     int numBlocks_x = (N + block_size_x - 1) / (block_size_x);
     //int numBlocks_x = (N + block_size_y * block_size_x - 1) / (block_size_x * block_size_y);
-    int numBlocks_y = (M + block_size_y * block_size_x - 1) / (block_size_x * block_size_y); */
+    int numBlocks_y = (M + block_size_y * block_size_x - 1) / (block_size_x * block_size_y);
     //printf("myGEMM is called!\n");
     dim3 threads(block_size_x, block_size_y);
     dim3 blocks(numBlocks_x, numBlocks_y);
-    device_gemm_shared<<<blocks, threads>>>(A, B, C, al, be, M, N, K);
+    device_gemm_shared2<<<blocks, threads>>>(A, B, C, al, be, M, N, K);
 
     
     return 0;
