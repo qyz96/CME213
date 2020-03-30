6d2d9e08923737ccf6dd6252d2f958888d6ff235
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-03-29 17:20:40 -0700

    Update gpu_func.cu

--- final_project/gpu_func.cu
+++ final_project/gpu_func.cu
@@ -7,7 +7,7 @@
 #include <math.h>
 #include "cublas_v2.h"
 #define BLOCK_SIZE 32
-#define BLOCK_SIZE_X 16
+#define BLOCK_SIZE_X 4
 #define BLOCK_SIZE_Y 4
 #define BLOCK_SIZE_K 32
 #define BLOCK_SIZE_Z 16
