e28595fc84c0a7720e370400be6803e593dcf8f9
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-03-31 04:47:42 -0700

    Update neural_network.cpp

--- final_project/neural_network.cpp
+++ final_project/neural_network.cpp
@@ -726,6 +726,7 @@ class OneBatchUpdateBonus  {
         cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, num_sample, K, &alpha, W1, N, a0, K, &zeta, z1, N);
         check_launch("myGEMM 2");
         double* dz1 = (double*)malloc(sizeof(double)*N*num_sample);
+        std::cout<<N<<" "<<num_sample<<"\n";
         cudaError_t err = cudaMemcpy(dz1, z1, sizeof(double) * N * num_sample, cudaMemcpyDeviceToHost);
         if(err != cudaSuccess) {
             std::cerr << "Error copying z1 to CPU" << std::endl;
