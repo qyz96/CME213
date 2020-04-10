402e3eddec8e5e8d0cd3ebe12a82b1120b7bcd36
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-04-10 06:52:05 -0700

    Update gpu_func.cu

--- final_project/gpu_func.cu
+++ final_project/gpu_func.cu
@@ -154,7 +154,7 @@ void device_gemm_shared2(double* __restrict__ A, double* __restrict__ B,
     int ri = threadIdx.y;
     int row = ri * BLOCK_SIZE_X + rj;
     int i = blockIdx.y * BLOCK_SIZE_Y * BLOCK_SIZE_X + row;
-    __shared__ double Bs[BLOCK_SIZE_Y][BLOCK_SIZE_X];
+    __shared__ double Bs[BLOCK_SIZE_Y][BLOCK_SIZE_X+1];
 
     double As[BLOCK_SIZE_Y];
     double temp[BLOCK_SIZE_X]={0};
@@ -207,7 +207,7 @@ void device_gemm_shared3(double* __restrict__ A, double* __restrict__ B,
     int ri = threadIdx.y;
     int row = ri * BLOCK_SIZE_Y + rj;
     int i = blockIdx.y * BLOCK_SIZE_Y * BLOCK_SIZE_X + row;
-    __shared__ double Bs[BLOCK_SIZE_X][BLOCK_SIZE_Y];
+    __shared__ double Bs[BLOCK_SIZE_X][BLOCK_SIZE_Y+1];
 
     double As[BLOCK_SIZE_X];
     double temp[BLOCK_SIZE_Y]={0};
@@ -264,14 +264,14 @@ int myGEMM(double* __restrict__ A, double* __restrict__ B,
     double al=*alpha;
     double be=*beta;
     
-/*     int block_size_x = BLOCK_SIZE;
+    int block_size_x = BLOCK_SIZE;
     int block_size_y = BLOCK_SIZE;
     int numBlocks_x = (N + block_size_x - 1) / block_size_x;
     int numBlocks_y = (M + block_size_y - 1) / (block_size_y); 
     dim3 threads(block_size_x, block_size_y);
     dim3 blocks(numBlocks_x, numBlocks_y);
     device_gemm_shared<<<blocks, threads>>>(A, B, C, al, be, M, N, K);
-    return 0; */
+    return 0;
     
     if (N>=BLOCK_SIZE_X) {
         int block_size_x = BLOCK_SIZE_X;
